ff728bcaf2e832b13f25d63d6060526bbe5dbf9a
pentester7374 <sundeep.amitmaurya@gmail.com>
2022-12-09 06:03:05 +0000

    edit

--- attack.c
+++ attack.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+ 
 static void malicious() __attribute__((constructor));
-
-void malicious(){
-	system("sleep 20");
+ 
+void malicious() {
+    system("sleep 20");
 }
